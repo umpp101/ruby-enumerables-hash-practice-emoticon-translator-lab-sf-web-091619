187ed8e705994756febed5f48820ed0e4088627d
Mustafa Muse <umpp101@icloud.com>
2019-09-13 22:41:43 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ end
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   library = load_library(file_path)
   if library["get_emoticon"].include?(english_emoticon) 
-    library["get_emoticon"][english_emoticon]
+    return library["get_emoticon"][english_emoticon]
   else
     "Sorry, that emoticon was not found"
   end
@@ -24,7 +24,7 @@ end
 def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   library = load_library(file_path)
   if library["get_meaning"].include?(japanese_emoticon) 
-    library["get_meaning"][japanese_emoticon]
+    return library["get_meaning"][japanese_emoticon]
   else
     "Sorry, that emoticon was not found"
   end
